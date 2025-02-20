699bac82fe7b916283c4914ea36f8273e3328531
Sceadu <44327463+sceadu37@users.noreply.github.com>
2025-02-20 11:44:35 -0500

    added a hurt process

--- Scripts/Mob/Mob.gd
+++ Scripts/Mob/Mob.gd
@@ -3,35 +3,50 @@ extends CharacterBody2D
 @export var awareness: float = 0.6
 @export var aggression = 0
 @export var fear = 0
-@export var health = 1
+@export var health: float = 10
 var alive: bool = true
+# prevents multiple hits in quick succession
+var invincible: bool = false
 
 var max_detection = 500
 var detection_range = max_detection * awareness
 
 @onready var player = $"../Player/PlayerBody"
 @onready var nav = $NavigationAgent2D
+@onready var sprite = $AnimatedSprite2D
 
 @export var speed = 40
 @export var accel = 7
 
 func die():
 	alive = false
-	$AnimatedSprite2D.material.set_shader_parameter("alive", alive)
-	$AnimatedSprite2D.material.set_shader_parameter("red_factor", 0.5)
-	$AnimatedSprite2D.material.set_shader_parameter("green_factor", 0.5)
-	$AnimatedSprite2D.material.set_shader_parameter("blue_factor", 0.5)
-	$AnimatedSprite2D.play("death")
+	sprite.material.set_shader_parameter("alive", alive)
+	sprite.material.set_shader_parameter("red_factor", 0.5)
+	sprite.material.set_shader_parameter("green_factor", 0.5)
+	sprite.material.set_shader_parameter("blue_factor", 0.5)
+	sprite.play("death")
 	
 	#func attack():
 		
 		
+# Function to run every time the mob is injured
+# Takes the amount of damage as an argument
+func hurt(damage: float):
+	if not invincible:
+		invincible = true
+		velocity = velocity/4.0
+		print("Hurt")
+		sprite.play("hurt")
+		health -= damage
+		$HitCooldown.start()
 		
+	
+
 
 func _physics_process(delta: float) -> void:
-	
+	#print($HitCooldown.time_left)
 	# kills mob if health is 0
-	if health == 0 and alive:
+	if health <= 0 and alive:
 		die()
 	
 	if alive:
@@ -43,34 +58,37 @@ func _physics_process(delta: float) -> void:
 		
 		#print(rotation/PI)
 		
-		# persue if within aggro range
-		if nav.distance_to_target() <= detection_range:
-			# create a vector of length 1 pointing towards the next pathfinding point
-			direction = nav.get_next_path_position() - global_position
-			direction = direction.normalized()
-			
-			# linear interpolation to apply acceleration
-			velocity = velocity.lerp(direction * speed, accel * delta)
-			# rotate sprite to match movement direction
-			rotation = velocity.angle()
-				
-		# return to idle
-		elif velocity.length() != 0:
-			#decelerate towards 0
-			velocity = velocity.lerp(Vector2.ZERO, accel/2 * delta)
-			#rotate towards 0; will be replaced with an "idle" animation & movement pattern
-			rotation = clampf(rotation - rotation * accel/2 * delta, min(0, rotation), max(0, rotation))
-		
-		if velocity.length() != 0:
-			$AnimatedSprite2D.play("swim")
+		if $HitCooldown.time_left > 0:
+			sprite.play("hurt")
 		else:
-			$AnimatedSprite2D.play("idle")
+			# persue if within aggro range
+			if nav.distance_to_target() <= detection_range:
+				# create a vector of length 1 pointing towards the next pathfinding point
+				direction = nav.get_next_path_position() - global_position
+				direction = direction.normalized()
+				
+				# linear interpolation to apply acceleration
+				velocity = velocity.lerp(direction * speed, accel * delta)
+				# rotate sprite to match movement direction
+				rotation = velocity.angle()
+					
+			# return to idle
+			elif velocity.length() != 0:
+				#decelerate towards 0
+				velocity = velocity.lerp(Vector2.ZERO, accel/2 * delta)
+				#rotate towards 0; will be replaced with an "idle" animation & movement pattern
+				rotation = clampf(rotation - rotation * accel/2 * delta, min(0, rotation), max(0, rotation))
+			
+			if velocity.length() != 0:
+				sprite.play("swim")
+			else:
+				sprite.play("idle")
 		
 		# flip sprite only on vertical axis bc rotation takes care of the horizontal flip
 		if rotation > PI/2 or rotation < -PI/2:
-			$AnimatedSprite2D.flip_v = true
+			sprite.flip_v = true
 		else:
-			$AnimatedSprite2D.flip_v = false	
+			sprite.flip_v = false	
 	
 	# if dead, drift to cave floor
 	else:
@@ -83,7 +101,14 @@ func _physics_process(delta: float) -> void:
 	for index in get_slide_collision_count():
 		var collision := get_slide_collision(index)
 		var body := collision.get_collider()
-		print("Collided with: ", body.name)
+		#print("Collided with: ", body.name)
 		if body.name == "PlayerBody":
-			print("	Collided with a player")
-			health = 0
+			#print("	Collided with a player")
+			hurt(1)
+
+# 
+func _on_hit_cooldown_timeout() -> void:
+	invincible = false
+	$HitCooldown.stop()
+	print("timer timeout")
+	
