0a33d732b1b5057961b76d5979ac902856590363
aaronperkel <aaron.perkel27@gmail.com>
2025-03-10 15:15:51 -0400

    working on #11

--- Scripts/Inventory/InventoryTest.gd
+++ Scripts/Inventory/InventoryTest.gd
@@ -4,20 +4,23 @@ extends Node2D
 func _ready():
 	print("Testing Inventory System...")
 	
-	# Create inventories for boat and person
 	var boat_storage = Inventory.new(40, 20)
 	var person_inventory = Inventory.new(6, 4)
 	
-	# Create a sample item and try adding it to the person's inventory
 	var wrench = Item.new("tool", "wrench", Vector2(0, 0), 3)
-	var add_success = person_inventory.add_item(wrench, Vector2(2, 1))
+	var hawk_tuah = Item.new("tool", "hawk tuah", Vector2(0, 0), 100)
+	
+	var add_1 = (person_inventory.add_item(wrench, Vector2(2, 1)))
+	var add_2 = (person_inventory.add_item(hawk_tuah, Vector2(3, 3)))
+	var add_success = add_1 and add_2
 	
 	if add_success:
-		print("Test passed: wrench added to person inventory at (2,1)")
+		print("Test passed:")
+		print("Wrench added to person inventory at (2,1)")
+		print("Wrench added to person inventory at (3,3)")
 	else:
-		print("Test failed: could not add wrench to person inventory")
+		print("Test failed: could not add items to person inventory")
 	
-	# Optionally, test moving or removing items
 	var move_success = person_inventory.move_item(Vector2(2, 1), Vector2(3, 2))
 	if move_success:
 		print("Test passed: wrench moved to (3,2)")
