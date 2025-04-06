41daaec5bde05477c243eec4885c5cd267a52bdf
jmathies1 <jakob.mathieson@uvm.edu>
2025-04-05 23:24:36 -0400

    Created rough flashlight shader, doesn't work on mob

--- Shaders/ShaderContext.gd
+++ Shaders/ShaderContext.gd
@@ -9,7 +9,13 @@ func _ready():
 	map_material.set_shader_parameter("IN_GAME", true)
 	mob_material.set_shader_parameter("IN_GAME", true)
 
-func _process(delta: float) -> void:
+func _process(_delta):
 	var screen_position = get_global_transform_with_canvas() * player.global_position
 	map_material.set_shader_parameter("player_pos", screen_position)
 	mob_material.set_shader_parameter("player_pos", screen_position)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		var mouse_position = event.position
+		map_material.set_shader_parameter("mouse_pos", mouse_position)
+		mob_material.set_shader_parameter("mouse_pos", mouse_position)
