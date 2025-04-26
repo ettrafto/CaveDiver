b7aa785316ff8b94ddb74d93bfa7edca99864b56
BenQuack <bquacken@uvm.edu>
2025-04-26 14:49:39 -0400

    rope spawning commit 2 messed up the first

--- Scripts/Map/rope_anchor.gd
+++ Scripts/Map/rope_anchor.gd
@@ -3,27 +3,26 @@ extends StaticBody2D
 var rope_segment_scene = preload("res://Things/rope_segment.tscn")
 var rope_segments = []
 var player_attached_rope
-
+var attached_player = null
 var can_start_rope = false
 
 func start_rope(player):
-	if not player.is_attached_to_rope():
-		player.set_rope_attachment(true)
-		rope_segments.append(rope_segment_scene.instantiate())
-		add_child(rope_segments[0])
-		rope_segments[0].set_pin_a(self.get_path())
-		
-		for i in range(3):
-			rope_segments.append(rope_segment_scene.instantiate())
-			add_child(rope_segments[-1])
-			rope_segments[-1].global_position = rope_segments[-2].get_bottom_node().global_position
-			rope_segments[-1].set_pin_a(rope_segments[-2].get_rigidBody().get_path())
-			
-		player_attached_rope = rope_segment_scene.instantiate()
-		add_child(player_attached_rope)
-		player_attached_rope.global_position = rope_segments[-1].get_bottom_node().global_position
-		player_attached_rope.set_pin_a(rope_segments[-1].get_rigidBody().get_path())
-		player_attached_rope.add_pinJoint(player)
+	attached_player = player
+	
+	rope_segments.append(rope_segment_scene.instantiate())
+	player_attached_rope = rope_segment_scene.instantiate()
+	rope_segments[0].remove_pin_joint()
+	#set up the first rope segment
+	self.add_child(rope_segments[0])
+	$DampedSpringJoint2D.node_b = rope_segments[0].get_rigidBody().get_path()
+	rope_segments[0].face_towards(self, player)
+	
+	#set up the rope that will stay attached to the player
+	self.add_child(player_attached_rope)
+	player_attached_rope.set_pos(rope_segments[0].get_bottom_pos())
+	player_attached_rope.set_pin_a(rope_segments[0].get_rigidBody().get_path())
+	player_attached_rope.face_towards(rope_segments[0].get_bottom_node(), player)
+	player_attached_rope.add_pin_joint(player)
 	
 	
 func extend_rope():
@@ -36,10 +35,13 @@ func extend_rope():
 func set_can_start(boolean):
 	can_start_rope = boolean
 
-func _on_area_2d_body_entered(body: RigidBody2D):
-	if body.name == "Player":
-		can_start_rope = true
-		self.start_rope(body)
+func _on_area_2d_body_entered(body: Node2D):
+	if body.name == "Player" and player_attached_rope == null:
+		if not body.is_attached_to_rope():
+			can_start_rope = true
+			self.start_rope(body)
 
-func _on_area_2d_body_exited(body):
-	can_start_rope = false
+func _on_body_detection_area_body_exited(body: RigidBody2D):
+	if len(rope_segments) > 2:
+		extend_rope()
+		
--- Scripts/Map/rope_segment.gd
+++ Scripts/Map/rope_segment.gd
@@ -2,9 +2,6 @@ extends Node2D
 
 var has_two_joints = false
 
-func _ready():
-	$PinJoint2D.disable_collision = true
-
 func set_pin_a(node_path):
 	$PinJoint2D.set_node_a(node_path)
 	
@@ -15,27 +12,34 @@ func get_rigidBody():
 	return $RigidBody2D
 	
 func get_top_pos():
-	return self.position
+	return self.global_position
+	
+func get_bottom_pos():
+	return $RigidBody2D/bottom_pos.global_position
 	
 func get_bottom_node():
 	return $RigidBody2D/bottom_pos
 	
 func set_pos(new_position):
-	self.position = new_position
+	self.global_position = new_position
 	
 func set_second_joint_pin(node_path):
 		$SecondJoint.set_node_b(node_path)
 
-# this function is meant to be used once both pins are set
-func set_bottom_pos(node):
-	pass
+# sets the bottom node position by setting the rotation towards the destiantion node
+func face_towards(start, end):
+	var distance = end.global_position - start.global_position #makes a vector and then aligns with it 
+	self.rotation += Vector2.DOWN.angle_to(distance)#segment starts pointing down
 	
-	
-func remove_pin():
+func remove_pin_joint():
 	remove_child($PinJoint2D)
 	
+func remove_bottom_pin_joint():
+	if has_two_joints:
+		remove_child($SeecondJoint)
+		has_two_joints = false
 	
-func add_pinJoint(player):
+func add_pin_joint(player):
 	has_two_joints = true
 	var new_joint = PinJoint2D.new()
 	new_joint.disable_collision = true
@@ -43,5 +47,4 @@ func add_pinJoint(player):
 	new_joint.name = "SecondJoint"
 	new_joint.position = get_bottom_node().position
 	new_joint.node_a = self.get_rigidBody().get_path()
-	player.global_position = $RigidBody2D/bottom_pos.global_position
 	new_joint.node_b = player.get_path()
