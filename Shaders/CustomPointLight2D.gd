b622ef3f69809f9f157a1d0246b5dc0e5eee3a9e
jmathies1 <jakob.mathieson@uvm.edu>
2025-04-26 23:51:07 -0400

    made it so point lights can be optionally set to oscillate in intensity

--- Shaders/CustomPointLight2D.gd
+++ Shaders/CustomPointLight2D.gd
@@ -1,11 +1,30 @@
 extends Node2D
 class_name CustomPointLight2D
 
-# radius of the point light's effectiveness in pixels
+## point light's effective radius
 @export var radius: float = 100.0
-# lighting multiplier of RGB components
+## increases brightness of light
 @export var intensity: float = 1.0
+
+## toggles whether the light's intensity will oscillate
+@export var oscillates: bool = false
+
+## frequency of oscillation in degrees
+@export_range(1, 360, 1) var frequency: int = 1
+
+## phase offset of oscillation in degrees
+@export var offset: int = 0
+
+var time: float = 0
+
+func _process(delta):
+	time += delta
 
 func get_vec4():
 	var screen_position = get_canvas_transform() * global_position
-	return Vector4(screen_position.x, screen_position.y, radius, intensity)
+	if (oscillates):
+		var oscillation = (cos(time * deg_to_rad(frequency) + deg_to_rad(offset)) + 1) / 2
+		var new_intensity = intensity * oscillation
+		return Vector4(screen_position.x, screen_position.y, radius, new_intensity)
+	else:
+		return Vector4(screen_position.x, screen_position.y, radius, intensity)
