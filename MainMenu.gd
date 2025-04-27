7dad43e6e15eeabaedd4e3302bfdb1fcca6a384a
aaronperkel <me@aaronperkel.com>
2025-04-26 23:28:32 -0400

    trying to figure out mouse control;

--- MainMenu.gd
+++ MainMenu.gd
@@ -2,49 +2,58 @@
 extends Control
 
 func _ready():
-	show_screen("MainMenu")
+	mouse_filter = MOUSE_FILTER_PASS
+	print("🔥 MainMenu.gd loaded, Start node is: ", $StartMenu/Start)
+	show_screen("StartMenu")
 
 func show_screen(screen_name: String) -> void:
-	# Loops over all direct children and sets only the panel with name
-	# "screen_name" to visible.
-	for child in get_children():
-		if child is Control:
-			child.visible = (child.name == screen_name)
+	for panel in get_children():
+		if panel is Control:
+			panel.visible = (panel.name == screen_name)
 
-# Main Menu callbacks
+#
+# StartMenu
+#
 func _on_Start_pressed() -> void:
+	print("🎉 Start pressed!")
 	show_screen("SaveMenu")
 
 func _on_Options_pressed() -> void:
-	# Options not implemented yet.
-	print("Options pressed")
+	print("Options not hooked up yet")
 
 func _on_Quit_pressed() -> void:
 	get_tree().quit()
 
-func _on_NewGame_pressed():
-	# tear down the menu
-	queue_free()
-	# un-pause and swap scenes
-	get_tree().paused = false
-	get_tree().change_scene_to_file("res://main_scene.tscn")
-
-# Demo Menu callbacks
-func _on_DemoLevel_pressed() -> void:
-	show_screen("PreDiveMenu")
-
-func _on_Shop_pressed() -> void:
-	# Not implemented yet.
-	print("Shop pressed")
-
-func _on_Storage_pressed() -> void:
-	# Not implemented yet.
-	print("Storage pressed")
-
-# Pre Dive Menu callbacks
+#
+# SaveMenu
+#
+func _on_Save1_pressed() -> void:
+	show_screen("ShopStorageMenu")
+func _on_Save2_pressed() -> void:
+	show_screen("ShopStorageMenu")
+func _on_Save3_pressed() -> void:
+	show_screen("ShopStorageMenu")
 func _on_Back_pressed() -> void:
-	show_screen("DemoMenu")
+	show_screen("StartMenu")
 
+#
+# ShopStorageMenu
+#
+func _on_DemoLevel_pressed() -> void:
+	show_screen("PrediveMenu")
+func _on_Shop_pressed() -> void:
+	# TODO: your shop logic
+	pass
+func _on_Storage_pressed() -> void:
+	# TODO: your storage logic
+	pass
+func _on_Back2_pressed() -> void:
+	show_screen("SaveMenu")
+
+#
+# PrediveMenu
+#
 func _on_Dive_pressed() -> void:
-	# Replace "res://main_scene.tscn" with the path to your game scene.
 	get_tree().change_scene("res://main_scene.tscn")
+func _on_Back3_pressed() -> void:
+	show_screen("ShopStorageMenu")
