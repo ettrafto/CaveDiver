96dc76f10b8ff06aae268d2f2b88f013ac050c34
jmathies1 <jakob.mathieson@uvm.edu>
2025-05-04 14:58:47 -0400

    added illumination layers (custom light masks) to mobs and CustomPointLight2Ds

--- Shaders/CustomPointLight2D.gd
+++ Shaders/CustomPointLight2D.gd
@@ -1,13 +1,22 @@
 extends Node2D
 class_name CustomPointLight2D
 
-# Light influence
 ## point light's effective radius
 @export_range(1, 1000, 1) var radius: float = 100.0
 ## increases brightness of light
 @export_range(0, 10, 0.01) var intensity: float = 1.0
 
-# Light oscillation 
+@export_flags("General", "Player Emitted", "Mob Emitted", "Misc") var illumination_mask = 1
+
+@export_category("Color")
+## red offset
+@export_range(0, 1, 0.01) var red: float = 0
+## green offset
+@export_range(0, 1, 0.01) var green: float = 0
+## blue offset
+@export_range(0, 1, 0.01) var blue: float = 0
+
+@export_category("Oscillation")
 ## toggles whether the light's intensity will oscillate
 @export var oscillates: bool = false
 ## frequency of oscillation in degrees
@@ -15,14 +24,6 @@ class_name CustomPointLight2D
 ## phase offset of oscillation in degrees
 @export var offset: int = 0
 var time: float = 0
-
-# RGB Components
-## red offset
-@export_range(0, 1, 0.01) var red: float = 0
-## green offset
-@export_range(0, 1, 0.01) var green: float = 0
-## blue offset
-@export_range(0, 1, 0.01) var blue: float = 0
 
 func _process(delta):
 	time += delta
@@ -38,3 +39,6 @@ func get_vec4():
 
 func get_RGB():
 	return Vector3(red, green, blue)
+
+func get_illumination_mask():
+	return illumination_mask
--- Shaders/ShaderContext.gd
+++ Shaders/ShaderContext.gd
@@ -19,6 +19,12 @@ func get_point_light_colors():
 		point_light_colors.append(point_light.get_RGB())
 	return point_light_colors
 
+func get_point_light_illumination_masks():
+	var point_light_masks: Array[int] = []
+	for point_light: CustomPointLight2D in point_lights:
+		point_light_masks.append(point_light.get_illumination_mask())
+	return point_light_masks
+
 func _ready():
 	RenderingServer.global_shader_parameter_set("in_game", true)
 	var num_point_lights = len(point_lights)
@@ -35,9 +41,13 @@ func _process(_delta):
 	map_material.set_shader_parameter("point_light_info", point_light_info)
 	mob_material.set_shader_parameter("point_light_info", point_light_info)
 	
-	var point_Light_colors = get_point_light_colors()
-	map_material.set_shader_parameter("point_light_colors", point_Light_colors)
-	mob_material.set_shader_parameter("point_light_colors", point_Light_colors)
+	var point_light_colors = get_point_light_colors()
+	map_material.set_shader_parameter("point_light_colors", point_light_colors)
+	mob_material.set_shader_parameter("point_light_colors", point_light_colors)
+	
+	var point_light_illumination_masks = get_point_light_illumination_masks()
+	map_material.set_shader_parameter("point_light_illumination_masks", point_light_illumination_masks)
+	mob_material.set_shader_parameter("point_light_illumination_masks", point_light_illumination_masks)
 
 func _input(event):
 	if event is InputEventMouseMotion:
