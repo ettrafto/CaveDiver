1b7c5ed1c2102c127cf21a975a3ca3ece98d5306
Sceadu <44327463+sceadu37@users.noreply.github.com>
2025-05-09 18:50:47 -0400

    made spear stick into mobs

--- Scripts/Player/spear.gd
+++ Scripts/Player/spear.gd
@@ -17,3 +17,18 @@ func _on_physics_collison_area_body_entered(body: Node2D) -> void:
 	linear_velocity = Vector2.ZERO
 	set_deferred("freeze",true)
 	$ProjectileBubbles.emitting = false
+
+func _on_hitbox_area_entered(area: Area2D) -> void:
+	print("Spear hitbox collision with, ", area.name)
+	if area.name == "Hurtbox":
+		print("selected new parent")
+		# get the spear's position relative to the parent of the hurtbox
+		var new_parent := area.get_parent()
+		$Sprite2D.reparent(new_parent, true)
+		print("new parent: ", get_parent().name)
+		
+		$PhysicsCollisionArea.monitorable = false
+		$PhysicsCollisionArea.monitoring = false
+		$Hitbox.monitoring = false
+		$Hitbox.monitorable = false
+		queue_free()
